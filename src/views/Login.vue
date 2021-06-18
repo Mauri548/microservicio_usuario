88a6d2f5d480922b95436b5ed217e8b6c7a070db
emanuelfleitas <emanuelfleitas2020@gmail.com>
2021-06-18 10:43:54 -0300

    cambios

---
+++ src/views/Login.vue
@@ -0,0 +1,75 @@
+<template>
+   
+  
+        <div class="column  container has-background-warning">
+
+            <div class="column">
+
+                <img src="../assets/logo_crenein.png" width="200" height="200" alt="">
+
+            </div>
+        
+          
+            <div class="column  has-background-white" >
+
+
+            <div class="columns ">
+                <div class="column has-background-primary is-2  pt-5 has-text-right is-flex-grow-0">
+                    <div class="field">
+                        <label class="label">Email</label>
+                    </div>
+                </div>
+                <div class="column has-background-info is-3  ">
+                    <div class="control">
+                        <input class="input is-size-5" type="text" placeholder="Email">
+                    </div>
+                </div>
+            </div>
+             <div class="columns is-2 ">
+                <div class="column has-background-primary is-2  pt-5 has-text-right is-flex-grow-0">
+                    <div class="field">
+                        <label class="label">Password</label>
+                    </div>
+                </div>
+                <div class="column has-background-info is-3  ">
+                    <div class="control">
+                        <input class="input is-size-5" type="text" placeholder="Password">
+                    </div>
+                </div>
+            </div>
+            <div class="columns has-background-warning">
+                
+                <div class="column has-background-info is-3  ">
+                    <button>Login</button>
+                </div>
+            
+            </div>
+
+
+
+
+
+
+
+
+
+            </div>
+      
+
+
+        </div>
+         
+
+</template>
+
+<script>
+export default {
+    name:'Login',
+}
+</script>
+
+<style>
+
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css";
+
+</style>
