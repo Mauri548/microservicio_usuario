fbde49ac6d188ea4e192f47d1fc50fa96c061e0b
Mauri548 <mauricioferreyra548@gmail.com>
2021-06-18 11:37:56 -0300

    inicio

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,14 @@
 <template>
-  <div class="home">
-  
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    
+  <div>
+    <h1>Inicio</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
