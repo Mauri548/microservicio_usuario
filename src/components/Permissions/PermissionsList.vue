d4b7f0e964217639de39826f15b427017ac5ee62
Mauri548 <mauricioferreyra548@gmail.com>
2021-08-07 18:14:42 -0300

    permission listo

--- src/components/Permissions/PermissionsList.vue
+++ src/components/Permissions/PermissionsList.vue
@@ -1,61 +1,32 @@
 <template>
     <div>
-        <button class="button btn-crenein">
-            <span>PuWiC permissions</span>
+        <button @click="activePermissionApp(data.id)" class="button btn-crenein">
+            <span>{{data.app}} permissions</span>
             <span class="icon is-small">
                 <i class="fas fa-chevron-down"></i>
             </span>
         </button>
-        <div>
+        <div v-show="data.activo" class="contenedor">
             <div class="conteiner-head mt-3">
                 <div class="buttons">
                     <ButtonIcon class="is-success has-text-weight-semibold" positionIcon="left" icon="far fa-save" >Save</ButtonIcon>
-                    <!-- <button class="button is-success has-text-weight-semibold">
-                        <span class="icon is-small">
-                            <i class="far fa-save"></i>
-                        </span>
-                        <span>Save</span>
-                    </button> -->
                     <ButtonIcon class="btn-crenein" icon="far fa-check-square" >Select all</ButtonIcon>
-                    <!-- <button class="button btn-crenein">
-                        <span>Select all</span>
-                        <span class="icon is-small">
-                            <i class="far fa-check-square"></i>
-                        </span>    
-                    </button> -->
                     <ButtonIcon class="btn-crenein" icon="far fa-square" >Clear selected</ButtonIcon>
-                    <!-- <button class="button btn-crenein">
-                        <span>Clear selected</span>
-                        <span class="icon is-small">
-                            <i class="far fa-square"></i>
-                        </span>
-                    </button> -->
                 </div>
                 <Searcher/>
             </div>
-            <div class="conteiner-body my-4">
+            <div v-for="permission in data.permissions" :key="permission.name" class="conteiner-body my-4">
                 <div>
-                    <div class="conteiner-head has-background-light p-2">
-                        <h3>Client list (6/6)</h3>
+                    <div @click="activarHeader(data,permission)" class="conteiner-head has-background-light p-2">
+                        <h3>{{permission.name}} ({{permission.permissions_activo}}/{{permission.total}})</h3>
                         <div class="buttons">
                             <ButtonIcon class="btn-crenein px-2" icon="far fa-lg fa-check-square" classIcon="mx-0"/>
                             <ButtonIcon class="btn-crenein px-2" icon="far fa-lg fa-square" classIcon="mx-0" />
                         </div>
                         <Searcher/>
                     </div>
-                    <div>
-                        <li>
-                            <span>See list</span>
-                            
-                        </li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                    <div v-if="permission.activo" class="column is-flex is-flex-wrap-wrap is-align-items-center" style="border: 1px solid #cccccc78">
+                        <PermissionsItem v-for="item in permission.lista" :key="item.name" :appId="data.id" :permissionId="permission.id" :item="item" @onActiveButton="activeButton" />
                     </div>
                 </div>
             </div>
@@ -66,18 +37,45 @@
 <script>
 import Searcher from '../Board/Searcher.vue'
 import ButtonIcon from '../Buttons/ButtonIcon.vue'
+import PermissionsItem from './PermissionsItem.vue'
 export default {
     name: 'PermissionsList',
     components: {
         Searcher,
         ButtonIcon,
+        PermissionsItem,
+    },
+    props: ['data'],
+    emits: ['onActiveButton','onActivePermissionApp','onActiveList'],
+    setup(props,{emit}) {
+        const activeButton = (e) => {
+            emit("onActiveButton",e)
+        }
+
+        const activePermissionApp = (id) => {
+            emit("onActivePermissionApp",id)
+        }
+
+        // Funcion que pasa al padre el id de la app principal y de la lista que le pertenece 
+        // Para modificar el valor y visualizar la vista
+        const activarHeader = (app, permission) => {
+            emit("onActiveList", app.id, permission.id)
+        }
+
+        return {
+            activeButton,
+            activePermissionApp,
+            activarHeader,
+        }
     }
 }
 </script>
 
 <style scoped>
-
-.conteiner-head, .conteiner-head2 {
+.contenedor {
+    transition: 0.9s ease;
+}
+.conteiner-head {
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -91,12 +89,51 @@ export default {
 .conteiner-head input {
     width: 40%;
 }
+.conteiner-body div .conteiner-head:hover {
+    cursor: pointer;
+}
 
 .color-succes {
     background: #28A745;
 }
 
-/* .conteiner-body {
+
+
+
+.switch-button {
+    display: inline-block;
+}
+
+.switch-button .switch-button__checkbox {
+    display: none;
+}
+.switch-button .switch-button__label {
+    background-color: #fff;
     border: 1px solid #ccc;
-} */
+    width: 4rem;
+    height: 2rem;
+    /* border-radius: 3rem; */
+    border-radius: 5px;
+    display: inline-block;
+    position: relative;
+}
+.switch-button .switch-button__label:before {
+    transition: .2s;
+    display: block;
+    position: absolute;
+    width: 2rem;
+    height: 1.9rem;
+    background-color: red;
+    content: '';
+    /* border-radius: 50%; */
+    border-radius: 5px;
+}
+.switch-button .switch-button__checkbox:checked + .switch-button__label {
+    background-color:#48c774;
+    border: 1px solid #48c774
+}
+.switch-button .switch-button__checkbox:checked + .switch-button__label:before {
+    transform: translateX(2rem);
+}
+
 </style>
