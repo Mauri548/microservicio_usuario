184a516e9fc7e7d7604dd6811c050aad7af1fc39
Mauri548 <mauricioferreyra548@gmail.com>
2021-11-16 12:42:28 -0300

    created noFoundData component

---
+++ src/components/NoFoundData.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="is-flex is-justify-content-center">
+        <span class="has-text-grey is-size-4-tablet is-size-5-mobile">
+            {{$t('board.body.emptyBoard')}}
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NoFoundData'
+}
+</script>
+
+<style>
+
+</style>
